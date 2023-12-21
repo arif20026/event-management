4168756cce6395575ae09f8e892a9dcf93c55a81
arif20026 <arifislamipe026@gmail.com>
2023-12-21 21:27:00 +0600

    added about page and career page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,8 +5,68 @@ const About = () => {
         <div>
             <Navbar></Navbar>
 
-            <h3>Welcome to about section!</h3>
-            
+            <div className="m-10">
+
+                Welcome to <span className="font-bold">Eventpro</span> – Your Premier Destination for Educational Event Management!<br />
+
+                At Event Pro, we believe in the power of education and the impact of well-executed events. Our mission is to provide a seamless and unparalleled experience in managing educational events, ensuring that every gathering becomes an enriching and memorable occasion.<br />
+
+                
+                <div>
+                Event Pro is a dedicated team of event management professionals passionate about fostering knowledge-sharing, networking, and skill development. With years of experience in the field, we understand the unique needs of educational events and have mastered the art of bringing visions to life.<br />
+                </div>
+
+                <div className="my-4">
+                <span className="text-2xl text-blue-500 font-bold">What We Do</span><br />
+                <span className="text-xl font-bold">Comprehensive Event Planning:</span><br />
+                From workshops and webinars to conferences and training programs, we specialize in planning and executing a wide range of educational events. Our attention to detail ensures that each event is meticulously organized and tailored to meet the objectives of our clients.<br />
+                <span className="text-xl font-bold">Cutting-Edge Technology:</span><br />
+
+                
+                Leveraging the latest event management technologies, we provide seamless online and offline experiences. Our tech-savvy approach enhances engagement, accessibility, and overall event success.<br />
+                <span className="text-xl font-bold">Dedicated Support:</span><br />
+
+                
+                Your success is our priority. Our dedicated team offers end-to-end support, from conceptualization to post-event analysis. We work closely with our clients to understand their goals and deliver events that exceed expectations.
+                </div>
+
+                <div className="my-4">
+                <span className="text-2xl text-blue-500 font-bold">Our Values</span><br />
+
+                <span className="text-xl font-bold">Excellence:</span><br />
+                We are committed to delivering excellence in every aspect of event management. Our team strives for perfection to create events that leave a lasting impact.<br />
+
+                <span className="text-xl font-bold">Innovation:</span><br />
+                Embracing innovation is at the core of what we do. We stay ahead of industry trends, incorporating the latest technologies and strategies to deliver cutting-edge events..<br />
+
+                <span className="text-xl font-bold">Collaboration:</span><br />
+                Collaboration is key to success. We foster strong partnerships with clients, vendors, and stakeholders to ensure a collaborative and harmonious event planning process.<br />
+
+               
+                <br />
+                
+                </div>
+
+                <div >
+                <span className="text-2xl text-blue-500 font-bold">Why Choose Event Pro?</span> <br />
+                <span className="text-xl font-bold">Expertise:</span>
+                
+                <br />
+                Benefit from the expertise of a team that understands the nuances of educational event management. Our experience speaks for itself. <br />
+                <span className="text-xl font-bold">Customization:</span>
+                
+                <br />
+                We believe in the power of customization. Each event is unique, and we tailor our services to match the specific needs and goals of our clients.<br />
+                <span className="text-xl font-bold">Passion for Education:</span>
+                <br />
+                Education is not just an industry for us; it's a passion. We are driven by the desire to contribute to the growth and success of educational initiatives. <br />
+                </div>
+
+                Thank you for considering Event Pro as your trusted partner in educational event management. We look forward to the opportunity to collaborate with you and turn your vision into a memorable and impactful reality.
+
+                For inquiries and partnership opportunities, contact us today!
+            </div>
+
         </div>
     );
 };
--- src/Pages/Career/Career.jsx
+++ src/Pages/Career/Career.jsx
@@ -5,8 +5,41 @@ const Career = () => {
         <div>
             <Navbar></Navbar>
 
-            <h3>Welcome to career section!</h3>
-            
+            <section className="m-4 space-y-4">
+                <h1 className="text-2xl">Join Our Team at Event Pro</h1>
+
+                <article>
+                    <h2 className="text-2xl text-blue-500">Why Work at Event Pro?</h2>
+                    <ul>
+                        <li><strong>Exciting Challenges:</strong> Join a team that thrives on tackling exciting challenges.</li>
+                        <li><strong>Innovative Environment:</strong> Be part of an innovative and forward-thinking environment.</li>
+                        <li><strong>Collaborative Culture:</strong> Collaboration is at the heart of our culture.</li>
+                        <li><strong>Growth Opportunities:</strong> We believe in nurturing talent and providing growth opportunities.</li>
+                    </ul>
+                </article>
+
+                <article>
+                    <h2 className="text-2xl text-blue-500">Current Openings</h2>
+                    <ul className="space-y-4">
+                        <li><strong>Event Coordinator:</strong> Play a pivotal role in planning and executing educational events. <button className="btn btn-sm bg-blue-500">Apply now</button></li>
+                        <li><strong>Marketing Specialist:</strong> Contribute to the promotion of educational events.<button className="btn btn-sm bg-blue-500">Apply now</button></li>
+                        <li><strong>Technology Enthusiast:</strong> Join our tech team and implement cutting-edge solutions.<button className="btn btn-sm bg-blue-500">Apply now</button></li>
+                    </ul>
+                </article>
+
+
+                <article>
+                    <h2 className="text-2xl text-blue-500">Internship Programs</h2>
+                    <p>Explore our internship programs designed to provide students and recent graduates with valuable insights into the world of educational events.</p>
+                </article>
+
+                <article>
+                    <h2 className="text-2xl text-blue-500">Join Event Pro in Shaping the Future of Educational Events</h2>
+                    <p>If you are ready to contribute to our mission and be part of a dynamic team, we invite you to explore career opportunities with us. Grow with us. Innovate with us. Create with us. Join Event Pro!</p>
+                    <p>For inquiries and applications, contact us at <a href="mailto:careers@eventpro.com" className="font-bold text-blue-500">careers@eventpro.com</a>.</p>
+                </article>
+            </section>
+
         </div>
     );
 };
