e699ed7b80478ffb1e699eb6334f08ec17dcebd6
arif20026 <arifislamipe026@gmail.com>
2023-12-22 10:26:08 +0600

    added grand final project

--- src/Pages/shared/Navbar/Navbar.jsx
+++ src/Pages/shared/Navbar/Navbar.jsx
@@ -3,9 +3,8 @@ import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../Provider/AuthProvider";
 import userDefaultPic from '../../../assets/user.png'
 import { FaGoogle } from "react-icons/fa";
+const Navbar= () => {
 
-
-const Navbar = () => {
 
     const navLinks = <>
         <li><NavLink to='/'>Home</NavLink></li>
@@ -17,7 +16,7 @@ const Navbar = () => {
     const navigate = useNavigate()
     const location = useLocation()
 
-    const { user, logOut,signInWithGoogle } = useContext(AuthContext)
+    const { user, logOut, signInWithGoogle } = useContext(AuthContext)
     const handleLogOut = () => {
         logOut()
             .then(() => console.log('user logged out successfully'))
@@ -26,65 +25,72 @@ const Navbar = () => {
 
     const handleSignInWithGoogle = () => {
         signInWithGoogle()
-        .then(result => {
-            console.log(result)
+            .then(result => {
+                console.log(result)
 
-            navigate(location?.state ? location.state : "/")
+                navigate(location?.state ? location.state : "/")
 
-        })
-        .catch(error => console.log(error))
+            })
+            .catch(error => console.log(error))
     }
+
+
+
     return (
-        <div>
+        <div className="navbar bg-gray-600   ">
+            <div className="navbar-start">
 
-
-
-
-            <div className="navbar bg-gray-600 text-white ">
-
-                <div className="navbar-start">
-                    <img src="https://i.ibb.co/rswm63W/Screenshot-302.png" alt="" className="w-10 h-10 rounded-full mr-2" />
-                 <h1 className="text-2xl">Eventpro</h1>
+                <div className="flex ">
+                <img src="https://i.ibb.co/rswm63W/Screenshot-302.png" alt="" className="w-10 h-10 rounded-full mr-2" />
+                 <h1 className="text-2xl text-white ">Eventpro</h1>
                 </div>
 
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className=" btn btn-sm ml-4  lg:hidden  bg-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         {navLinks}
                     </ul>
                 </div>
-                <div className="navbar-end gap-2">
-                    <div className=" flex justify-center items-center gap-2 ">
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-white">
+                    {navLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+
+                <div className=" flex justify-center items-center gap-2 text-white  ">
 
 
-                        {
-                            user ? <>
-                                <p>{user.displayName}</p>
-                                <img src={user.photoURL} alt="" className="w-12 rounded-full" />
+                    {
+                        user ? <>
+                            <p>{user.displayName}</p>
+                            <img src={user.photoURL} alt="" className="w-12 rounded-full mr-1" />
 
-                            </> : <img src={userDefaultPic} alt="" className="w-12 rounded-full" />
-                        }
+                        </> : <img src={userDefaultPic} alt="" className="w-12 rounded-full" />
+                    }
 
-                    </div>
-                    <div className="">
-                        {
-                            user ?
-
-                                <Link to='/'> <button className="btn btn-primary w-20" onClick={handleLogOut}>LogOut</button></Link>
-
-                                :
-                                <>
-                                    <Link to='/login'> <button className="btn btn-primary w-20" >Login</button></Link>
-                                    <button className="btn btn-secondary ml-2   " onClick={handleSignInWithGoogle}>
-                                        <FaGoogle />
-                                        Sign in with google
-                                    </button>
-
-                                </>
-
-                        }
-
-                    </div>
                 </div>
+
+                {
+                    user ?
+
+                        <Link to='/'> <button className="btn btn-primary w-20" onClick={handleLogOut}>LogOut</button></Link>
+
+                        :
+                        <>
+                            <Link to='/login'> <button className="btn btn-primary w-20" >Login</button></Link>
+                            <button className="btn btn-secondary ml-2   " onClick={handleSignInWithGoogle}>
+                                <FaGoogle />
+                                Sign in with google
+                            </button>
+
+                        </>
+
+                }
+
 
             </div>
         </div>
